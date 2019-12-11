f51479d5ad387ae84e2077a616851689eeebc31f
OverEarth12 <Yama_Karaito@hotmail.com>
2019-12-11 16:22:17 +0700

    add logout system and keep current user

---
+++ public.html/login.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+document.getElementById("idLogin").onclick = function() {
+
+        var email = document.getElementById('idUser').value;
+        var password = document.getElementById('idPassword').value;
+
+        firebase.auth().signInWithEmailAndPassword(email + '@it.kmitl.ac.th', password)
+        .then(function(){
+                window.location.replace("ChoosePage.html");
+                console.log('success');
+                localStorage.setItem("currentuse", email);
+            }).catch(function(error) {
+                alert('login fail');
+                console.log('error');
+            })}}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById('currentUser').innerHTML = "<tr><td>" + user.email + "</td><td>"
+  } else {
+    // No user is signed in.
+  }
+});
+
+function logout() {
+    localStorage.removeItem('currentuse');
+    firebase.auth().signOut().then(function() {
+      console.log('Signed Out');
+    }, function(error) {
+      console.error('Have something wrong');
+    });
+}
